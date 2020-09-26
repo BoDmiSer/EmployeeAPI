74da6e55d21b47ea8be939e5ed30d19bdcb63c56
Дмитрий Богданов <bogdanov_s69@mail.ru>
2020-09-26 23:14:06 +0300

    Добавьте файлы проекта.

---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        #region Properties
+        [Required(ErrorMessage = "IdRequired")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "NameRequired")]
+        [StringLength(64, MinimumLength = 2)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "SurNameRequierd")]
+        [StringLength(64, MinimumLength = 2)]
+        [Display(Name = "SurName")]
+        public string SurName { get; set; }
+
+        [StringLength(64, MinimumLength = 2)]
+        [Display(Name = "Patronymic")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "DateRequired")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
+        public DateTime RegistrationDate { get; set; }
+
+        [StringLength(256, MinimumLength = 2)]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+        #endregion  
+    }
+}
---
+++ EmployeeAPI/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddControllersWithViews().AddDataAnnotationsLocalization();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var supportedCultures = new[]
+           {
+                new CultureInfo("en"),
+                new CultureInfo("ru")
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ru"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
